84477d2bd283bf57350e2e8bc9eca6def5ec03b6
Catie Jo <catiejo@caties-mbp.zhaw.ch>
2018-11-08 09:50:53 +0100

    starting Day Picker Activity

---
+++ app/src/main/java/com/zhaw/catiejo/whatsforlunch/DayPickerActivity.java
@@ -0,0 +1,13 @@
+package com.zhaw.catiejo.whatsforlunch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DayPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_picker);
+    }
+}
